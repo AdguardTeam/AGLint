1246f4de4e26c607342fe2c8e6be3287b7d66ca8
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2022-11-28 19:53:55 +0100

    Remove redundant config comment

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
