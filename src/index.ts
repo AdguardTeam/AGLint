b2171d3cb3f1ff2a66db56134218f0c6d928054a
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-01 11:11:19 +0100

    Export CLI stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 /** AGLint package root */
 
+// Parser
 export * from "./parser";
+
+// Core linter
 export * from "./linter";
+
+// Linter CLI
+export * from "./linter/cli";
+export * from "./linter/cli/config-reader";
+export * from "./linter/cli/constants";
+export * from "./linter/cli/reporter";
+export * from "./linter/cli/scan";
+export * from "./linter/cli/walk";
