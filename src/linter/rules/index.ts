e14ac13993b1acc3e3786965c894d500855804b8
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-14 14:31:19 +0100

    Liter rule that detects unknown preprocessor directives

--- src/linter/rules/index.ts
+++ src/linter/rules/index.ts
@@ -2,9 +2,11 @@ import { LinterRule } from "../rule";
 import { AdgScriptletQuotes } from "./adg-scriptlet-quotes";
 import { IfClosed } from "./if-closed";
 import { SingleSelector } from "./single-selector";
+import { UnknownPreProcessorDirectives } from "./unknown-preprocessor-directives";
 
 export const defaultLinterRules = new Map<string, LinterRule>([
     ["adg-scriptlet-quotes", AdgScriptletQuotes],
     ["if-closed", IfClosed],
     ["single-selector", SingleSelector],
+    ["unknown-preprocessor-directives", UnknownPreProcessorDirectives],
 ]);
