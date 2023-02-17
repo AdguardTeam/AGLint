aacd0f6776d82eb031d697bf82fda66eb083ad4a
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-17 20:49:16 +0100

    Refactor to intersection

--- src/linter/rules/inconsistent-hint-platforms.ts
+++ src/linter/rules/inconsistent-hint-platforms.ts
@@ -8,12 +8,10 @@ import { AnyRule } from "../../parser";
 import { RuleCategory } from "../../parser/categories";
 import { CommentRuleType } from "../../parser/comment/types";
 import { EMPTY } from "../../utils/constants";
+import { ArrayUtils } from "../../utils/array";
 
 const PLATFORM = "PLATFORM";
 const NOT_PLATFORM = "NOT_PLATFORM";
-
-const FLAG_TARGETED = 1;
-const FLAG_EXCLUDED = 2;
 
 /**
  * Rule that checks if a platform targeted by a PLATFORM() hint is also excluded by a NOT_PLATFORM()
@@ -37,11 +35,10 @@ export const InconsistentHintPlatforms = <LinterRule>{
                     return;
                 }
 
-                // Store all affected platforms in an object, where the key is the platform name and the
-                // value is a flag number, where
-                //  - the first bit indicates if the platform is targeted by a PLATFORM hint and
-                //  - the second bit indicates if the platform is excluded by a NOT_PLATFORM hint
-                const platforms: { [key: string]: number } = {};
+                // Collect platforms targeted and excluded by PLATFORM() and NOT_PLATFORM() hints,
+                // so we can compare them later
+                const targeted: string[] = [];
+                const excluded: string[] = [];
 
                 // Iterate over all hints within the comment rule
                 for (const hint of ast.hints) {
@@ -51,33 +48,33 @@ export const InconsistentHintPlatforms = <LinterRule>{
                         // which has no parameters practically
                         if (hint.params.length > 1 || (hint.params.length === 1 && hint.params[0] !== EMPTY)) {
                             for (const param of hint.params) {
-                                // Add the platform to the table if it's not already there
-                                if (!(param in platforms)) {
-                                    platforms[param] = hint.name === PLATFORM ? FLAG_TARGETED : FLAG_EXCLUDED;
+                                if (hint.name === PLATFORM) {
+                                    // Platform targeted by a PLATFORM() hint
+                                    targeted.push(param);
                                 } else {
-                                    // Update the platforms table (leave the other value untouched)
-                                    platforms[param] |= hint.name === PLATFORM ? FLAG_TARGETED : FLAG_EXCLUDED;
+                                    // Platform excluded by a NOT_PLATFORM() hint
+                                    excluded.push(param);
                                 }
                             }
                         }
                     }
                 }
 
-                // Report problems based on the platforms table
-                for (const [platform, flags] of Object.entries(platforms)) {
-                    // The platform targeted and excluded at the same time
-                    if (flags === (FLAG_TARGETED | FLAG_EXCLUDED)) {
-                        context.report({
-                            // eslint-disable-next-line max-len
-                            message: `Platform "${platform}" is targeted by a PLATFORM() hint and excluded by a NOT_PLATFORM() hint at the same time`,
-                            position: {
-                                startLine: line,
-                                startColumn: 0,
-                                endLine: line,
-                                endColumn: raw.length,
-                            },
-                        });
-                    }
+                // Get platforms that are targeted and excluded at the same time
+                const intersection = ArrayUtils.getIntersection(targeted, excluded);
+
+                // Report problems
+                for (const platform of intersection) {
+                    context.report({
+                        // eslint-disable-next-line max-len
+                        message: `Platform "${platform}" is targeted by a PLATFORM() hint and excluded by a NOT_PLATFORM() hint at the same time`,
+                        position: {
+                            startLine: line,
+                            startColumn: 0,
+                            endLine: line,
+                            endColumn: raw.length,
+                        },
+                    });
                 }
             }
         },
