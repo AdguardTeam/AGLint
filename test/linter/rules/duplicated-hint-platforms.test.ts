8fd94d833aca93085945a728e46197634774726d
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-20 15:36:30 +0100

    refactor test

--- test/linter/rules/duplicated-hint-platforms.test.ts
+++ test/linter/rules/duplicated-hint-platforms.test.ts
@@ -1,45 +1,70 @@
 import { Linter } from "../../../src/linter";
 import { DuplicatedHintPlatforms } from "../../../src/linter/rules/duplicated-hint-platforms";
-import { NEWLINE } from "../../../src/utils/constants";
+
+let linter: Linter;
 
 describe("duplicated-hint-platforms", () => {
-    test("Detects duplicated hint platforms", () => {
-        const linter = new Linter(false);
-
+    beforeAll(() => {
+        // Configure linter with the rule
+        linter = new Linter(false);
         linter.addRule("duplicated-hint-platforms", DuplicatedHintPlatforms);
+    });
 
-        // Problem-free cases
+    test("should ignore non-problematic cases", () => {
+        expect(linter.lint(`!+ PLATFORM(windows)`)).toMatchObject({ problems: [] });
         expect(
             linter.lint(
-                [
-                    `!+ PLATFORM(windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`,
-
-                    `!+ NOT_PLATFORM(windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ NOT_PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`,
-                ].join(NEWLINE)
+                // eslint-disable-next-line max-len
+                `!+ PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`
             )
-        ).toMatchObject({
-            problems: [],
-            warningCount: 0,
-            errorCount: 0,
-            fatalErrorCount: 0,
+        ).toMatchObject({ problems: [] });
+
+        expect(linter.lint(`!+ NOT_PLATFORM(windows)`)).toMatchObject({ problems: [] });
+        expect(
+            linter.lint(
+                // eslint-disable-next-line max-len
+                `!+ NOT_PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`
+            )
+        ).toMatchObject({ problems: [] });
+    });
+
+    it("should detect problematic cases", () => {
+        expect(linter.lint(`!+ PLATFORM(windows, windows)`)).toMatchObject({
+            problems: [
+                {
+                    rule: "duplicated-hint-platforms",
+                    severity: 1,
+                    message:
+                        // eslint-disable-next-line max-len
+                        'The platform "windows" is occurring more than once within the same "PLATFORM" hint',
+                    position: {
+                        startColumn: 0,
+                        endColumn: 29,
+                    },
+                },
+            ],
         });
 
-        // Problematic cases
+        expect(linter.lint(`!+ NOT_PLATFORM(windows, windows)`)).toMatchObject({
+            problems: [
+                {
+                    rule: "duplicated-hint-platforms",
+                    severity: 1,
+                    message:
+                        // eslint-disable-next-line max-len
+                        'The platform "windows" is occurring more than once within the same "NOT_PLATFORM" hint',
+                    position: {
+                        startColumn: 0,
+                        endColumn: 33,
+                    },
+                },
+            ],
+        });
+
         expect(
             linter.lint(
-                [
-                    `!+ PLATFORM(windows, windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ PLATFORM(windows, mac, android, ios, windows, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_safari, ext_ublock)`,
-
-                    `!+ NOT_PLATFORM(windows, windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ NOT_PLATFORM(windows, mac, android, ios, windows, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_safari, ext_ublock)`,
-                ].join(NEWLINE)
+                // eslint-disable-next-line max-len
+                `!+ PLATFORM(windows, mac, android, ios, windows, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_safari, ext_ublock)`
             )
         ).toMatchObject({
             problems: [
@@ -50,22 +75,7 @@ describe("duplicated-hint-platforms", () => {
                         // eslint-disable-next-line max-len
                         'The platform "windows" is occurring more than once within the same "PLATFORM" hint',
                     position: {
-                        startLine: 1,
                         startColumn: 0,
-                        endLine: 1,
-                        endColumn: 29,
-                    },
-                },
-                {
-                    rule: "duplicated-hint-platforms",
-                    severity: 1,
-                    message:
-                        // eslint-disable-next-line max-len
-                        'The platform "windows" is occurring more than once within the same "PLATFORM" hint',
-                    position: {
-                        startLine: 2,
-                        startColumn: 0,
-                        endLine: 2,
                         endColumn: 143,
                     },
                 },
@@ -76,12 +86,20 @@ describe("duplicated-hint-platforms", () => {
                         // eslint-disable-next-line max-len
                         'The platform "ext_safari" is occurring more than once within the same "PLATFORM" hint',
                     position: {
-                        startLine: 2,
                         startColumn: 0,
-                        endLine: 2,
                         endColumn: 143,
                     },
                 },
+            ],
+        });
+
+        expect(
+            linter.lint(
+                // eslint-disable-next-line max-len
+                `!+ NOT_PLATFORM(windows, mac, android, ios, windows, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_safari, ext_ublock)`
+            )
+        ).toMatchObject({
+            problems: [
                 {
                     rule: "duplicated-hint-platforms",
                     severity: 1,
@@ -89,22 +107,7 @@ describe("duplicated-hint-platforms", () => {
                         // eslint-disable-next-line max-len
                         'The platform "windows" is occurring more than once within the same "NOT_PLATFORM" hint',
                     position: {
-                        startLine: 3,
                         startColumn: 0,
-                        endLine: 3,
-                        endColumn: 33,
-                    },
-                },
-                {
-                    rule: "duplicated-hint-platforms",
-                    severity: 1,
-                    message:
-                        // eslint-disable-next-line max-len
-                        'The platform "windows" is occurring more than once within the same "NOT_PLATFORM" hint',
-                    position: {
-                        startLine: 4,
-                        startColumn: 0,
-                        endLine: 4,
                         endColumn: 147,
                     },
                 },
@@ -115,16 +118,11 @@ describe("duplicated-hint-platforms", () => {
                         // eslint-disable-next-line max-len
                         'The platform "ext_safari" is occurring more than once within the same "NOT_PLATFORM" hint',
                     position: {
-                        startLine: 4,
                         startColumn: 0,
-                        endLine: 4,
                         endColumn: 147,
                     },
                 },
             ],
-            warningCount: 6,
-            errorCount: 0,
-            fatalErrorCount: 0,
         });
     });
 });
