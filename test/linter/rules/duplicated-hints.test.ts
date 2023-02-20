2593072c603358531bce81179f4254bdbf38dc62
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-20 15:29:24 +0100

    refactor test

--- test/linter/rules/duplicated-hints.test.ts
+++ test/linter/rules/duplicated-hints.test.ts
@@ -1,49 +1,92 @@
 import { Linter } from "../../../src/linter";
 import { DuplicatedHints } from "../../../src/linter/rules/duplicated-hints";
-import { NEWLINE } from "../../../src/utils/constants";
+
+let linter: Linter;
 
 describe("duplicated-hints", () => {
-    test("Detects duplicated hints", () => {
-        const linter = new Linter(false);
-
+    beforeAll(() => {
+        // Configure linter with the rule
+        linter = new Linter(false);
         linter.addRule("duplicated-hints", DuplicatedHints);
+    });
 
-        // Problem-free cases
+    test("should ignore non-problematic cases", () => {
+        // PLATFORM with single parameter
+        expect(linter.lint(`!+ PLATFORM(windows)`)).toMatchObject({ problems: [] });
+
+        // PLATFORM with multiple parameters
         expect(
             linter.lint(
-                [
-                    `!+ PLATFORM(windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`,
-
-                    `!+ NOT_PLATFORM(windows)`,
-                    // eslint-disable-next-line max-len
-                    `!+ NOT_PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`,
-
-                    `!+ NOT_OPTIMIZED PLATFORM(windows)`,
-
-                    `!+ PLATFORM(windows) NOT_PLATFORM(mac)`,
-                    `!+ PLATFORM(mac) NOT_PLATFORM(windows)`,
-                ].join(NEWLINE)
+                // eslint-disable-next-line max-len
+                `!+ PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`
             )
-        ).toMatchObject({
-            problems: [],
-            warningCount: 0,
-            errorCount: 0,
-            fatalErrorCount: 0,
+        ).toMatchObject({ problems: [] });
+
+        // NOT_PLATFORM with single parameter
+        expect(linter.lint(`!+ NOT_PLATFORM(windows)`)).toMatchObject({ problems: [] });
+
+        // NOT_PLATFORM with multiple parameters
+        expect(
+            linter.lint(
+                // eslint-disable-next-line max-len
+                `!+ NOT_PLATFORM(windows, mac, android, ios, ext_chromium, ext_ff, ext_edge, ext_opera, ext_safari, ext_android_cb, ext_ublock)`
+            )
+        ).toMatchObject({ problems: [] });
+
+        // Multiple hints within the same rule, but no duplicates
+        expect(linter.lint(`!+ NOT_OPTIMIZED PLATFORM(windows)`)).toMatchObject({ problems: [] });
+        expect(linter.lint(`!+ PLATFORM(windows) NOT_PLATFORM(mac)`)).toMatchObject({ problems: [] });
+        expect(linter.lint(`!+ PLATFORM(mac) NOT_PLATFORM(windows)`)).toMatchObject({ problems: [] });
+    });
+
+    it("should detect problematic cases", () => {
+        expect(linter.lint(`!+ PLATFORM(windows) PLATFORM(mac) NOT_PLATFORM(android) NOT_OPTIMIZED`)).toMatchObject({
+            problems: [
+                {
+                    rule: "duplicated-hints",
+                    severity: 1,
+                    message: 'The hint "PLATFORM" is occurring more than once within the same comment rule',
+                    position: {
+                        startColumn: 0,
+                        endColumn: 70,
+                    },
+                },
+            ],
         });
 
-        // Problematic cases
+        expect(linter.lint(`!+ NOT_OPTIMIZED NOT_OPTIMIZED PLATFORM(windows)`)).toMatchObject({
+            problems: [
+                {
+                    rule: "duplicated-hints",
+                    severity: 1,
+                    message: 'The hint "NOT_OPTIMIZED" is occurring more than once within the same comment rule',
+                    position: {
+                        startColumn: 0,
+                        endColumn: 48,
+                    },
+                },
+            ],
+        });
+
+        expect(
+            linter.lint(`!+ PLATFORM(windows) NOT_PLATFORM(ext_ff) NOT_PLATFORM(mac) NOT_PLATFORM(android)`)
+        ).toMatchObject({
+            problems: [
+                {
+                    rule: "duplicated-hints",
+                    severity: 1,
+                    message: 'The hint "NOT_PLATFORM" is occurring more than once within the same comment rule',
+                    position: {
+                        startColumn: 0,
+                        endColumn: 81,
+                    },
+                },
+            ],
+        });
+
         expect(
             linter.lint(
-                [
-                    `!+ PLATFORM(windows) PLATFORM(mac) NOT_PLATFORM(android) NOT_OPTIMIZED`,
-                    `!+ NOT_OPTIMIZED NOT_OPTIMIZED PLATFORM(windows)`,
-                    `!+ PLATFORM(windows) NOT_PLATFORM(ext_ff) NOT_PLATFORM(mac) NOT_PLATFORM(android)`,
-                    // eslint-disable-next-line max-len
-                    `!+ PLATFORM(windows) NOT_PLATFORM(ext_ff) NOT_PLATFORM(mac) NOT_PLATFORM(android) PLATFORM(ext_edge)`,
-                    `!+ HINT HINT(aaa) HINT() HINT(aaa, bbb, ccc)`,
-                ].join(NEWLINE)
+                `!+ PLATFORM(windows) NOT_PLATFORM(ext_ff) NOT_PLATFORM(mac) NOT_PLATFORM(android) PLATFORM(ext_edge)`
             )
         ).toMatchObject({
             problems: [
@@ -52,42 +95,7 @@ describe("duplicated-hints", () => {
                     severity: 1,
                     message: 'The hint "PLATFORM" is occurring more than once within the same comment rule',
                     position: {
-                        startLine: 1,
                         startColumn: 0,
-                        endLine: 1,
-                        endColumn: 70,
-                    },
-                },
-                {
-                    rule: "duplicated-hints",
-                    severity: 1,
-                    message: 'The hint "NOT_OPTIMIZED" is occurring more than once within the same comment rule',
-                    position: {
-                        startLine: 2,
-                        startColumn: 0,
-                        endLine: 2,
-                        endColumn: 48,
-                    },
-                },
-                {
-                    rule: "duplicated-hints",
-                    severity: 1,
-                    message: 'The hint "NOT_PLATFORM" is occurring more than once within the same comment rule',
-                    position: {
-                        startLine: 3,
-                        startColumn: 0,
-                        endLine: 3,
-                        endColumn: 81,
-                    },
-                },
-                {
-                    rule: "duplicated-hints",
-                    severity: 1,
-                    message: 'The hint "PLATFORM" is occurring more than once within the same comment rule',
-                    position: {
-                        startLine: 4,
-                        startColumn: 0,
-                        endLine: 4,
                         endColumn: 100,
                     },
                 },
@@ -96,27 +104,25 @@ describe("duplicated-hints", () => {
                     severity: 1,
                     message: 'The hint "NOT_PLATFORM" is occurring more than once within the same comment rule',
                     position: {
-                        startLine: 4,
                         startColumn: 0,
-                        endLine: 4,
                         endColumn: 100,
                     },
                 },
+            ],
+        });
+
+        expect(linter.lint(`!+ HINT HINT(aaa) HINT() HINT(aaa, bbb, ccc)`)).toMatchObject({
+            problems: [
                 {
                     rule: "duplicated-hints",
                     severity: 1,
                     message: 'The hint "HINT" is occurring more than once within the same comment rule',
                     position: {
-                        startLine: 5,
                         startColumn: 0,
-                        endLine: 5,
                         endColumn: 44,
                     },
                 },
             ],
-            warningCount: 6,
-            errorCount: 0,
-            fatalErrorCount: 0,
         });
     });
 });
