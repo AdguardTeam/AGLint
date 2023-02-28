5e1ec9e32bb0a8a5c4e8c84664f0df9f00f0f8eb
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-02-28 11:03:33 +0100

    Refactor `duplicated-modifiers.test.ts`

--- test/linter/rules/duplicated-modifiers.test.ts
+++ test/linter/rules/duplicated-modifiers.test.ts
@@ -1,60 +1,50 @@
 import { Linter } from '../../../src/linter';
 import { DuplicatedModifiers } from '../../../src/linter/rules/duplicated-modifiers';
-import { NEWLINE } from '../../../src/utils/constants';
+
+let linter: Linter;
 
 describe('duplicated-modifiers', () => {
-    test('Detects duplicated modifiers', () => {
-        const linter = new Linter(false);
-
+    beforeAll(() => {
+        // Configure linter with the rule
+        linter = new Linter(false);
         linter.addRule('duplicated-modifiers', DuplicatedModifiers);
+    });
 
-        // No multiple selectors
-        expect(linter.lint(['example.com##.ad', '||example.com^$script,third-party'].join(NEWLINE))).toMatchObject({
-            problems: [],
-            warningCount: 0,
-            errorCount: 0,
-            fatalErrorCount: 0,
-        });
+    test('should ignore non-problematic cases', () => {
+        // Cosmetic rule
+        expect(linter.lint('example.com##.ad')).toMatchObject({ problems: [] });
 
-        expect(
-            linter.lint(['example.com##.ad', '||example.com^$script,third-party,script,script,script'].join(NEWLINE)),
-        ).toMatchObject({
+        // Network rule, but no duplicates
+        expect(linter.lint('||example.com^$script,third-party')).toMatchObject({ problems: [] });
+    });
+
+    it('should detect problematic cases', () => {
+        // 4x script
+        expect(linter.lint('||example.com^$script,third-party,script,script,script')).toMatchObject({
             problems: [
                 {
                     rule: 'duplicated-modifiers',
                     severity: 2,
                     message: 'The modifier "script" is used multiple times, but it should be used only once',
                     position: {
-                        startLine: 2,
                         startColumn: 0,
-                        endLine: 2,
                         endColumn: 54,
                     },
                 },
             ],
-            warningCount: 0,
-            errorCount: 1,
-            fatalErrorCount: 0,
         });
 
-        expect(
-            linter.lint(
-                [
-                    'example.com##.ad',
-                    '||example.com^$script,third-party,script,script,script,third-party,script,domain=example.net',
-                    'ads.js$script,domain=example.com,domain=example.net',
-                ].join(NEWLINE),
-            ),
-        ).toMatchObject({
+        // 5x script, 2x third-party
+        expect(linter.lint(
+            '||example.com^$script,third-party,script,script,script,third-party,script,domain=example.net',
+        )).toMatchObject({
             problems: [
                 {
                     rule: 'duplicated-modifiers',
                     severity: 2,
                     message: 'The modifier "script" is used multiple times, but it should be used only once',
                     position: {
-                        startLine: 2,
                         startColumn: 0,
-                        endLine: 2,
                         endColumn: 92,
                     },
                 },
@@ -63,27 +53,28 @@ describe('duplicated-modifiers', () => {
                     severity: 2,
                     message: 'The modifier "third-party" is used multiple times, but it should be used only once',
                     position: {
-                        startLine: 2,
                         startColumn: 0,
-                        endLine: 2,
                         endColumn: 92,
                     },
                 },
+            ],
+        });
+
+        // 2x domain
+        expect(linter.lint(
+            'ads.js$script,domain=example.com,domain=example.net',
+        )).toMatchObject({
+            problems: [
                 {
                     rule: 'duplicated-modifiers',
                     severity: 2,
                     message: 'The modifier "domain" is used multiple times, but it should be used only once',
                     position: {
-                        startLine: 3,
                         startColumn: 0,
-                        endLine: 3,
                         endColumn: 51,
                     },
                 },
             ],
-            warningCount: 0,
-            errorCount: 3,
-            fatalErrorCount: 0,
         });
     });
 });
