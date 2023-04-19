83498c5cc825e52a831b8026e0bc22db43a625c7
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-04-19 09:36:25 +0200

    create custom error

---
+++ src/linter/cli/errors/no-config-error.ts
@@ -0,0 +1,13 @@
+/**
+ * We throw this error when we can't find a config file for the given directory.
+ * It is easy to catch this error and show a nice error message to the user
+ * when using the CLI tool.
+ */
+export class NoConfigError extends Error {
+    constructor(dir: string) {
+        super(dir);
+
+        this.name = 'NoConfigError';
+        super(`No config file found for ${dir}`);
+    }
+}
