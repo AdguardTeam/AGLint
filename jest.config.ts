40a380cc1227f168cda55da99f428e93270bdba8
scripthunter7 <d.tota@adguard.com>
2023-06-28 11:27:51 +0200

    Restore comment

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
     testEnvironment: 'node',
     testTimeout: 30000,
     testMatch: ['**/test/**/*.test.ts'],
+    // Speed up tests by using SWC instead of Babel
     transform: { '.ts': '@swc/jest' },
 };
 
