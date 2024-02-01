9f8e64843aecacfc204f5ed566175d9f3627e43f
scripthunter7 <d.tota@adguard.com>
2024-02-01 16:40:21 +0100

    update rule

--- src/linter/rules/no-invalid-network-rule-pattern.ts
+++ src/linter/rules/no-invalid-network-rule-pattern.ts
@@ -8,56 +8,6 @@ import { StringUtils } from '../../utils/string';
 import { getErrorMessage } from '../../utils/error';
 
 /**
- * Validates network rule pattern.
- *
- * @param pattern Pattern to validate.
- * @returns `true` if pattern is valid, error message otherwise.
- */
-const validateNetworkRulePattern = (pattern: string): true | string => {
-    // pattern may a regular expression
-    if (StringUtils.isRegexPattern(pattern)) {
-        try {
-            regexpTree.parse(pattern);
-        } catch (error: unknown) {
-            return `Invalid regular expression: ${getErrorMessage(error)}`;
-        }
-
-        return true;
-    }
-
-    let i = 0;
-
-    // pattern may start with protocol mask (||) or begin pointer (|)
-    if (pattern[i] === PIPE) {
-        i += 1;
-
-        if (pattern[i] === PIPE) {
-            i += 1;
-        }
-    }
-
-    // pattern may end with an ending pointer (|)
-    // we check it here to avoid checking it in the loop
-    let end = pattern.length - 1;
-
-    if (pattern[end] === PIPE) {
-        end -= 1;
-    }
-
-    // iterate over the pattern
-    while (i < end) {
-        // if we found pipe or space or tab we should report an error
-        if (pattern[i] === PIPE || StringUtils.isWhitespace(pattern[i])) {
-            return 'Unexpected pipe or whitespace character';
-        }
-
-        i += 1;
-    }
-
-    return true;
-};
-
-/**
  * Rule that checks if network rule pattern is valid.
  */
 export const NoInvalidNetworkRulePattern: LinterRule = {
@@ -69,12 +19,74 @@ export const NoInvalidNetworkRulePattern: LinterRule = {
             const node = context.getActualAdblockRuleAst();
 
             if (node.category === RuleCategory.Network) {
-                const validationResult = validateNetworkRulePattern(node.pattern.value);
-                if (validationResult !== true) {
-                    context.report({
-                        message: validationResult,
-                        node: node.pattern,
-                    });
+                const pattern = node.pattern.value;
+
+                // pattern may a regular expression
+                if (StringUtils.isRegexPattern(pattern)) {
+                    try {
+                        regexpTree.parse(pattern);
+                    } catch (error: unknown) {
+                        context.report({
+                            message: `Invalid regular expression: ${getErrorMessage(error)}`,
+                            node: node.pattern,
+                        });
+                    }
+
+                    return;
+                }
+
+                let i = 0;
+
+                // pattern may start with protocol mask (||) or begin pointer (|)
+                if (pattern[i] === PIPE) {
+                    i += 1;
+
+                    if (pattern[i] === PIPE) {
+                        i += 1;
+                    }
+                }
+
+                // pattern may end with an ending pointer (|)
+                // we check it here to avoid checking it in the loop
+                let end = pattern.length - 1;
+
+                if (pattern[end] === PIPE) {
+                    end -= 1;
+                }
+
+                // iterate over the pattern and check for invalid characters
+                while (i < end) {
+                    if (pattern[i] === PIPE) {
+                        context.report({
+                            message: 'Unexpected pipe character',
+                            position: {
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                startLine: node.loc!.start.line,
+                                startColumn: i,
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                endLine: node.loc!.end.line,
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                endColumn: node.loc!.end.column - 1,
+                            },
+                        });
+                    }
+
+                    if (StringUtils.isWhitespace(pattern[i])) {
+                        context.report({
+                            message: 'Unexpected whitespace character',
+                            position: {
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                startLine: node.loc!.start.line,
+                                startColumn: i,
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                endLine: node.loc!.end.line,
+                                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                endColumn: node.loc!.end.column - 1,
+                            },
+                        });
+                    }
+
+                    i += 1;
                 }
             }
         },
