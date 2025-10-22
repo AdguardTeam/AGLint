db2a3048c9c999e89fc33c61fbde435d56f769cb
Elizaveta Egorova <e.egorova@adguard.com>
2025-10-23 00:40:25 +0300

    AG-46023 Add rule for checking quotes type in scriptlet
    Squashed commit of the following:
    
    commit 9d107005a925fe0132f27a58f038fea721e122f6
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Thu Oct 23 00:36:33 2025 +0300
    
        update
    
    commit 2f4c0655e65f2c891bc02e5b4d2ebd614b96cb7c
    Merge: e7cdcf8 fc9bc1c
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Thu Oct 23 00:33:31 2025 +0300
    
        Merge branch 'release/v4.0' into fix/AG-46023
    
    commit e7cdcf859e84a2343d1f8cbd994088f058f0c4f1
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Wed Oct 22 14:21:02 2025 +0300
    
        add empty lines
    
    commit 7dc94f68d56ed634ddb782dd964243e6a9ca2ae3
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Tue Oct 21 23:09:17 2025 +0300
    
        add tests
    
    commit b2eb17cc5efcf48e3a7fa88a358c60251b6b5ccd
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Mon Oct 20 13:04:57 2025 +0300
    
        update rule
    
    commit 9abe91e43979169e273f519ee9bd85e6375de161
    Merge: ad23d86 d0bc2b9
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Mon Oct 20 11:53:07 2025 +0300
    
        merge master
    
    commit ad23d86b4ea288a58779c163b1bb49d169b2c2bf
    Author: Elizaveta <e.egorova@adguard.com>
    Date:   Sat Oct 18 13:37:54 2025 +0300
    
        AG-46023 Add rule for checking quotes type in scriptlet

---
+++ src/rules/scriptlet-quotes.ts
@@ -0,0 +1,123 @@
+import {
+    AdblockSyntax,
+    QuoteType,
+    QuoteUtils,
+    type ScriptletInjectionRule,
+    type Value,
+} from '@adguard/agtree';
+import * as v from 'valibot';
+
+import { defineRule, LinterRuleType } from '../linter/rule';
+
+export const OPEN_CURLY_DOUBLE_QUOTE = '“';
+export const CLOSE_CURLY_DOUBLE_QUOTE = '”';
+export const OPEN_CURLY_SINGLE_QUOTE = '‘';
+export const CLOSE_CURLY_SINGLE_QUOTE = '’';
+
+/**
+ * Check if a string is curly (smart) quoted.
+ *
+ * @param value The string to check.
+ *
+ * @returns True if the string is curly quoted, false otherwise.
+ */
+const isCurlyQuoted = (value: string): boolean => {
+    return (value.startsWith(OPEN_CURLY_SINGLE_QUOTE) && value.endsWith(CLOSE_CURLY_SINGLE_QUOTE))
+        || (value.startsWith(OPEN_CURLY_DOUBLE_QUOTE) && value.endsWith(CLOSE_CURLY_DOUBLE_QUOTE));
+};
+
+export default defineRule({
+    meta: {
+        type: LinterRuleType.Problem,
+        docs: {
+            name: 'scriptlet-quotes',
+            description: 'Checks quotes in scriptlet',
+            recommended: true,
+        },
+        messages: {
+            unexpectedQuote: 'Scriptlet argument should be quoted with {{quote}}, but {{actualQuote}} was found',
+            curlyQuotesDisallowed: 'Curly quotes are disallowed',
+        },
+        configSchema: v.tuple([
+            v.object({
+                adg: v.enum(QuoteType),
+                ubo: v.enum(QuoteType),
+                abp: v.enum(QuoteType),
+                disallowCurlyQuotes: v.boolean(),
+            }),
+        ]),
+        hasFix: true,
+    },
+    create: (context) => {
+        let syntax: AdblockSyntax | undefined;
+        const checkScriptletArguments = (expectedQuoteType: QuoteType, scriptletArgument: Value<string>) => {
+            // special case: disallow curly quotes on demand
+            // note: QuoteUtils does not handle curly quotes, its just important at linter level,
+            // not at parser level
+            if (context.config[0].disallowCurlyQuotes && isCurlyQuoted(scriptletArgument.value)) {
+                context.report({
+                    messageId: 'curlyQuotesDisallowed',
+                    node: scriptletArgument,
+                    fix(fixer) {
+                        const range = context.getOffsetRangeForNode(scriptletArgument);
+                        if (!range) {
+                            return null;
+                        }
+                        const { value } = scriptletArgument;
+                        return fixer.replaceWithText(
+                            range,
+                            QuoteUtils.setStringQuoteType(value.slice(1, -1), expectedQuoteType),
+                        );
+                    },
+                });
+            }
+            // handle regular quotes
+            const actualQuoteType = QuoteUtils.getStringQuoteType(scriptletArgument.value);
+            if (actualQuoteType === expectedQuoteType) {
+                return;
+            }
+            context.report({
+                messageId: 'unexpectedQuote',
+                data: {
+                    quote: expectedQuoteType,
+                    actualQuote: actualQuoteType,
+                },
+                node: scriptletArgument,
+                fix(fixer) {
+                    const range = context.getOffsetRangeForNode(scriptletArgument);
+                    if (!range) {
+                        return null;
+                    }
+                    const { value } = scriptletArgument;
+
+                    return fixer.replaceWithText(
+                        range,
+                        QuoteUtils.setStringQuoteType(value, expectedQuoteType),
+                    );
+                },
+            });
+        };
+        return {
+            ScriptletInjectionRule: (node: ScriptletInjectionRule) => {
+                syntax = node.syntax;
+            },
+            'ScriptletInjectionRule:exit': () => {
+                syntax = undefined;
+            },
+            'ScriptletInjectionRule ParameterList Value': (node: Value<string>) => {
+                switch (syntax) {
+                    case AdblockSyntax.Adg:
+                        checkScriptletArguments(context.config[0].adg, node);
+                        break;
+                    case AdblockSyntax.Ubo:
+                        checkScriptletArguments(context.config[0].ubo, node);
+                        break;
+                    case AdblockSyntax.Abp:
+                        checkScriptletArguments(context.config[0].abp, node);
+                        break;
+                    default:
+                }
+            },
+        };
+    },
+});
---
+++ test/rules/scriptlet-quotes.test.ts
@@ -0,0 +1,203 @@
+import { QuoteType } from '@adguard/agtree';
+import { describe, expect, test } from 'vitest';
+
+import { type LinterRulesConfig } from '../../src/linter/config';
+import { LinterRuleSeverity } from '../../src/linter/rule';
+
+import { lint, lintWithFixes } from './helpers/lint';
+
+const rulesConfig: LinterRulesConfig = {
+    'scriptlet-quotes': [LinterRuleSeverity.Error, {
+        adg: QuoteType.Double, ubo: QuoteType.None, abp: QuoteType.None, disallowCurlyQuotes: true,
+    }],
+};
+
+describe('scriptlet-quotes', () => {
+    test('should ignore non-problematic cases', async () => {
+        expect((await lint(
+            'example.com,~example.net#%#//scriptlet("scriptlet0", "arg0", "arg1")',
+            rulesConfig,
+        )).problems).toStrictEqual([]);
+        expect((await lint(
+            'example.com,~example.net##+js(‘scriptlet0‘, ‘arg0‘, ‘arg1‘)',
+            rulesConfig,
+        )).problems).toStrictEqual([]);
+        expect((await lint(
+            'example.com,~example.net##+js(scriptlet0, arg0, arg1)',
+            rulesConfig,
+        )).problems).toStrictEqual([]);
+        expect((await lint(
+            'example.com,~example.net#$#scriptlet0 arg0 arg1',
+            rulesConfig,
+        )).problems).toStrictEqual([]);
+    });
+
+    test('should detect problematic cases', async () => {
+        expect((await lint(
+            "example.com,~example.net#%#//scriptlet('scriptlet0', 'arg0', 'arg1')",
+            rulesConfig,
+        )).problems).toMatchObject([
+            {
+                category: 'problem',
+                data: {
+                    actualQuote: 'single',
+                    quote: 'double',
+                },
+                fix: {
+                    range: [
+                        39,
+                        51,
+                    ],
+                    text: '"scriptlet0"',
+                },
+                messageId: 'unexpectedQuote',
+                position: {
+                    end: {
+                        column: 51,
+                        line: 1,
+                    },
+                    start: {
+                        column: 39,
+                        line: 1,
+                    },
+                },
+                ruleId: 'scriptlet-quotes',
+                severity: 2,
+            },
+            {
+                category: 'problem',
+                data: {
+                    actualQuote: 'single',
+                    quote: 'double',
+                },
+                fix: {
+                    range: [
+                        53,
+                        59,
+                    ],
+                    text: '"arg0"',
+                },
+                messageId: 'unexpectedQuote',
+                position: {
+                    end: {
+                        column: 59,
+                        line: 1,
+                    },
+                    start: {
+                        column: 53,
+                        line: 1,
+                    },
+                },
+                ruleId: 'scriptlet-quotes',
+                severity: 2,
+            },
+            {
+                category: 'problem',
+                data: {
+                    actualQuote: 'single',
+                    quote: 'double',
+                },
+                fix: {
+                    range: [
+                        61,
+                        67,
+                    ],
+                    text: '"arg1"',
+                },
+                messageId: 'unexpectedQuote',
+                position: {
+                    end: {
+                        column: 67,
+                        line: 1,
+                    },
+                    start: {
+                        column: 61,
+                        line: 1,
+                    },
+                },
+                ruleId: 'scriptlet-quotes',
+                severity: 2,
+            },
+        ]);
+    });
+
+    test('should detect problematic cases', async () => {
+        expect((await lint(
+            // eslint-disable-next-line @typescript-eslint/quotes
+            "example.com,~example.net#$#‘scriptlet0’ ‘arg0 arg1’",
+            rulesConfig,
+        )).problems).toMatchObject([{
+            category: 'problem',
+            data: undefined,
+            fix: {
+                range: [
+                    27,
+                    39,
+                ],
+                text: 'scriptlet0',
+            },
+            messageId: 'curlyQuotesDisallowed',
+            position: {
+                end: {
+                    column: 39,
+                    line: 1,
+                },
+                start: {
+                    column: 27,
+                    line: 1,
+                },
+            },
+            ruleId: 'scriptlet-quotes',
+            severity: 2,
+        }]);
+    });
+
+    test('should fix single quotes to double quotes in ADG scriptlets', async () => {
+        const result = await lintWithFixes(
+            "example.com,~example.net#%#//scriptlet('scriptlet0', 'arg0', 'arg1')",
+            rulesConfig,
+        );
+
+        expect(result.fixedSource).toBe(
+            'example.com,~example.net#%#//scriptlet("scriptlet0", "arg0", "arg1")',
+        );
+    });
+
+    test('should fix curly quotes in ABP scriptlets', async () => {
+        const result = await lintWithFixes(
+            // eslint-disable-next-line @typescript-eslint/quotes
+            "example.com,~example.net#$#'scriptlet0' 'arg0 arg1'",
+            rulesConfig,
+        );
+
+        expect(result.fixedSource).toBe(
+            'example.com,~example.net#$#scriptlet0 arg0 arg1',
+        );
+    });
+
+    test('should fix multiple quote issues in complex scriptlet', async () => {
+        const result = await lintWithFixes(
+            "example.com#%#//scriptlet('remove-class', 'ads', 'div')",
+            rulesConfig,
+        );
+
+        expect(result.fixedSource).toBe(
+            'example.com#%#//scriptlet("remove-class", "ads", "div")',
+        );
+    });
+
+    test('should not modify content when no fixes are needed', async () => {
+        const input = 'example.com,~example.net#%#//scriptlet("scriptlet0", "arg0", "arg1")';
+        const result = await lintWithFixes(input, rulesConfig);
+
+        expect(result.fixedSource).toBe(input);
+    });
+    test('should not modify content when no fixes are needed', async () => {
+        const input = 'example.com,~example.net##+js(‘scriptlet0’, ‘arg0’, ‘arg1’)';
+        const result = await lintWithFixes(input, rulesConfig);
+
+        expect(result.fixedSource).toBe(
+            'example.com,~example.net##+js(scriptlet0, arg0, arg1)',
+        );
+    });
+});
