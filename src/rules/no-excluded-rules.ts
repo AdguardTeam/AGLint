c136591a1092aafe1a8528845b50add9954557ad
scripthunter7 <d.tota@adguard.com>
2025-10-27 19:48:56 +0100

    exclude by rule text

--- src/rules/no-excluded-rules.ts
+++ src/rules/no-excluded-rules.ts
@@ -2,6 +2,7 @@ import { type AnyRule, RegExpUtils } from '@adguard/agtree';
 import * as v from 'valibot';
 
 import { defineRule, LinterRuleType } from '../linter/rule';
+import { createVisitorsForAnyValidRule } from '../utils/visitor-creator';
 
 export default defineRule({
     meta: {
@@ -12,12 +13,16 @@ export default defineRule({
             recommended: false,
         },
         messages: {
+            excludedRuleText: 'Rule matches an excluded rule text: {{ruleText}}',
             excludedPattern: 'Rule matches an excluded pattern: {{pattern}}',
         },
         // TODO: Improve schema with transformation: https://github.com/AdguardTeam/AGLint/issues/217)
         configSchema: v.tuple([
             v.strictObject({
-                regExpPatterns: v.pipe(
+                excludedRuleTexts: v.pipe(
+                    v.fallback(v.array(v.string()), []),
+                ),
+                excludedRegExpPatterns: v.pipe(
                     v.fallback(v.array(v.string()), []),
                 ),
             }),
@@ -28,7 +33,7 @@ export default defineRule({
         const cache: WeakMap<string[], RegExp[]> = new WeakMap();
 
         const getRegExpList = (): RegExp[] => {
-            const patterns = context.config[0].regExpPatterns;
+            const patterns = context.config[0].excludedRegExpPatterns;
 
             if (cache.has(patterns)) {
                 return cache.get(patterns)!;
@@ -50,6 +55,29 @@ export default defineRule({
         };
 
         const handler = (node: AnyRule) => {
+            const excludeRuleTexts = context.config[0].excludedRuleTexts;
+
+            if (excludeRuleTexts.includes(node.raws!.text!)) {
+                context.report({
+                    messageId: 'excludedRuleText',
+                    data: {
+                        ruleText: node.raws!.text!,
+                    },
+                    fix(fixer) {
+                        const lineNumber = context.sourceCode.getLineNumberForOffset(node.start!);
+                        if (!lineNumber) {
+                            return null;
+                        }
+                        const lineRange = context.sourceCode.getLineRange(lineNumber, true);
+                        if (!lineRange) {
+                            return null;
+                        }
+                        return fixer.remove(lineRange);
+                    },
+                    node,
+                });
+            }
+
             const excludePatterns = getRegExpList();
 
             for (const pattern of excludePatterns) {
@@ -77,13 +105,6 @@ export default defineRule({
             }
         };
 
-        return {
-            NetworkRule: handler,
-            CssInjectionRule: handler,
-            ElementHidingRule: handler,
-            ScriptletInjectionRule: handler,
-            HtmlFilteringRule: handler,
-            JsInjectionRule: handler,
-        };
+        return createVisitorsForAnyValidRule(handler);
     },
 });
--- test/rules/no-excluded-rules.test.ts
+++ test/rules/no-excluded-rules.test.ts
@@ -7,12 +7,284 @@ import { LinterRuleSeverity } from '../../src/linter/rule';
 import { lint } from './helpers/lint';
 
 describe('no-excluded-rules', () => {
+    test('should match exact excluded rule texts', async () => {
+        const rulesConfig: LinterRulesConfig = {
+            'no-excluded-rules': [
+                LinterRuleSeverity.Error,
+                {
+                    excludedRuleTexts: [
+                        'example.com/exact/match/',
+                    ],
+                },
+            ],
+        };
+
+        // Zero bad rules
+        expect(
+            (await lint(
+                [
+                    'example.com/good/query/',
+                    'example.com/another-not-bad/query/',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([]);
+
+        // One bad rule (exact match)
+        expect(
+            (await lint(
+                [
+                    'example.com/good/query/',
+                    'example.com/exact/match/',
+                    'example.com/another-not-bad/query/',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: 'example.com/exact/match/',
+                },
+                fix: {
+                    range: [24, 49],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: example.com/exact/match/',
+                position: {
+                    start: {
+                        line: 2,
+                        column: 0,
+                    },
+                    end: {
+                        line: 2,
+                        column: 24,
+                    },
+                },
+                category: 'problem',
+            },
+        ]);
+
+        // Multiple bad same rules
+        expect(
+            (await lint(
+                [
+                    'example.com/exact/match/',
+                    'example.com/good/query/',
+                    'example.com/exact/match/',
+                    'example.com/another-not-bad/query/',
+                    'example.com/exact/match/',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: 'example.com/exact/match/',
+                },
+                fix: {
+                    range: [0, 25],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: example.com/exact/match/',
+                position: {
+                    start: {
+                        line: 1,
+                        column: 0,
+                    },
+                    end: {
+                        line: 1,
+                        column: 24,
+                    },
+                },
+                category: 'problem',
+            },
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: 'example.com/exact/match/',
+                },
+                fix: {
+                    range: [49, 74],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: example.com/exact/match/',
+                position: {
+                    start: {
+                        line: 3,
+                        column: 0,
+                    },
+                    end: {
+                        line: 3,
+                        column: 24,
+                    },
+                },
+                category: 'problem',
+            },
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: 'example.com/exact/match/',
+                },
+                fix: {
+                    range: [109, 133],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: example.com/exact/match/',
+                position: {
+                    start: {
+                        line: 5,
+                        column: 0,
+                    },
+                    end: {
+                        line: 5,
+                        column: 24,
+                    },
+                },
+                category: 'problem',
+            },
+        ]);
+
+        // Should not match partial strings
+        expect(
+            (await lint(
+                [
+                    'example.com/exact/match/more',
+                    '||example.com/exact/match/',
+                    'example.com/exact/match',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([]);
+    });
+
+    test('should match multiple different excluded rule texts', async () => {
+        const rulesConfig: LinterRulesConfig = {
+            'no-excluded-rules': [
+                LinterRuleSeverity.Error,
+                {
+                    excludedRuleTexts: [
+                        'example.com##.ad',
+                        '||ads.example.com^',
+                        '@@||example.com/whitelist^',
+                    ],
+                },
+            ],
+        };
+
+        // Zero bad rules
+        expect(
+            (await lint(
+                [
+                    'example.com##.banner',
+                    '||tracking.example.com^',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([]);
+
+        // Three different bad rules
+        expect(
+            (await lint(
+                [
+                    'example.com##.ad',
+                    'example.com##.banner',
+                    '||ads.example.com^',
+                    '@@||example.com/whitelist^',
+                ].join(NEWLINE),
+                rulesConfig,
+            )).problems,
+        ).toStrictEqual([
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: 'example.com##.ad',
+                },
+                fix: {
+                    range: [0, 17],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: example.com##.ad',
+                position: {
+                    start: {
+                        line: 1,
+                        column: 0,
+                    },
+                    end: {
+                        line: 1,
+                        column: 16,
+                    },
+                },
+                category: 'problem',
+            },
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: '||ads.example.com^',
+                },
+                fix: {
+                    range: [38, 57],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: ||ads.example.com^',
+                position: {
+                    start: {
+                        line: 3,
+                        column: 0,
+                    },
+                    end: {
+                        line: 3,
+                        column: 18,
+                    },
+                },
+                category: 'problem',
+            },
+            {
+                ruleId: 'no-excluded-rules',
+                severity: LinterRuleSeverity.Error,
+                messageId: 'excludedRuleText',
+                data: {
+                    ruleText: '@@||example.com/whitelist^',
+                },
+                fix: {
+                    range: [57, 83],
+                    text: '',
+                },
+                message: 'Rule matches an excluded rule text: @@||example.com/whitelist^',
+                position: {
+                    start: {
+                        line: 4,
+                        column: 0,
+                    },
+                    end: {
+                        line: 4,
+                        column: 26,
+                    },
+                },
+                category: 'problem',
+            },
+        ]);
+    });
+
     test('should match one specified excluded rule', async () => {
         const rulesConfig: LinterRulesConfig = {
             'no-excluded-rules': [
                 LinterRuleSeverity.Error,
                 {
-                    regExpPatterns: [
+                    excludedRegExpPatterns: [
                         String.raw`example\.com\/bad\/query\/`,
                     ],
                 },
@@ -160,7 +432,7 @@ describe('no-excluded-rules', () => {
             'no-excluded-rules': [
                 LinterRuleSeverity.Error,
                 {
-                    regExpPatterns: [
+                    excludedRegExpPatterns: [
                         String.raw`example\.(com|org)\/bad\/query\/`,
                     ],
                 },
