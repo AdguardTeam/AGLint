4bbf66e5753cb7f34a5f6d524a43c695beaafe12
scripthunter7 <d.tota@adguard.com>
2025-11-03 20:36:14 +0100

    more exports

--- src/linter/index.ts
+++ src/linter/index.ts
@@ -1,12 +1,21 @@
-export type { LinterConfig } from './config';
+export type {
+    LinterConfig,
+    ParseFunction,
+    Parser,
+    GetOffsetFromNode,
+    LinterRulesConfig,
+    LinterConfigParsed,
+    DEFAULT_CHILD_KEY,
+    DEFAULT_TYPE_KEY,
+} from './config';
 export { lint, type LinterResult, type LinterRunOptions } from './linter';
 export {
     applyFixesToResult,
     lintWithFixes,
     type LinterFixerResult,
     type LinterFixerRunOptions,
+    type AnyLinterResult,
 } from './fixer';
-export { LinterRuleSeverity } from './rule';
 export { type LinterProblem, type LinterSuggestion } from './linter-problem';
 export { defaultSubParsers } from './default-subparsers';
 export { type LinterFixCommand } from './source-code/fix-generator';
@@ -15,3 +24,27 @@ export {
     type LinterPositionRange,
     type LinterOffsetRange,
 } from './source-code/source-code';
+export {
+    type LinterRule,
+    defineRule,
+    type LinterRuleContext,
+    type LinterRuleVisitors,
+    type LinterProblemReport,
+    type Suggestion,
+    defineConfigSchema,
+    type ConfigOf,
+    type LinterRuleBaseContext,
+    type LinterRuleConfig,
+    type LinterRuleExample,
+    LinterRuleType,
+    LinterRuleSeverity,
+} from './rule';
+export {
+    createVisitorsForAnyCommentRule,
+    createVisitorsForAnyCosmeticRule,
+    createVisitorsForAnyNetworkRule,
+    createVisitorsForAnyRule,
+    createVisitorsForAnyValidRule,
+} from './visitor-creator';
+export { type LinterRuleLoader } from './rule-registry/rule-loader';
+export { type LinterFileProps } from './file-props';
